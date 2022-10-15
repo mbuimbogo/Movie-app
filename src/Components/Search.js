1be5735a44b3a4cf84f61799e47b10618fd4c575
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-10-15 13:37:28 +0300

    Add SearchForm

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import { useState } from 'react'
 
-const Search = () => {
+const Search = (search) => {
+    const [searchValue, setSearchValue] = useState("");
   return (
-    <div>Search</div>
+    <form className='search'>
+        <input 
+        value={searchValue}
+        onChange={handleSearchInputChanges}
+        type="text"
+        />
+        <input onClick= {callSearchFunction} type="submit" value="SEARCH"></input>
+
+    </form>
   )
 }
 
