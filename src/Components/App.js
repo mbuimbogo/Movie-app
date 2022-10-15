6110acf8a2be289b3039b0441fb124c34681e76f
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-10-15 13:56:16 +0300

    setStates

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,6 +7,9 @@ import './App.css';
 
 function App() {
   const MOVI_API_URL = "https://www.omdbapi.com/?s=man&apikey=4a3b711b"
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null)
   return (
     <div className="App">
       <header className="App-header">
