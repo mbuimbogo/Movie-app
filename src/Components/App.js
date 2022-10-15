893e6180df91247822decb4fd2fcf438244b13db
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-10-15 14:02:37 +0300

    Fetch data from API

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import "../App.css"
 import {useState, useEffect} from "react";
 import Header from './Header';
 import Search from './Search';
 import Movie from './Movie';
-import './App.css';
+
 
 function App() {
   const MOVI_API_URL = "https://www.omdbapi.com/?s=man&apikey=4a3b711b"
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    fetch(MOVI_API_URL)
+    .then(response => response.json())
+    .then(data => console.log(movies))
+  })
+
   return (
     <div className="App">
       <header className="App-header">
